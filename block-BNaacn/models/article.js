6ffd2e70ce8e8dad1986e496ff0a822db97cc016
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-06-29 11:19:44 +0530

    Refactor code

--- block-BNaacn/models/article.js
+++ block-BNaacn/models/article.js
@@ -6,10 +6,10 @@ const articleSchema = new Schema({
     title: String,
     description: String, 
     tags: [String],
-    createAt: { type: Date, default: new Date()},
+    createdAt: { type: Date, default: new Date()},
     likes: {type: Number, default: 0} 
 });
 
 const Article = mongoose.model('Article', articleSchema);
 
-module.exports(Article);
+module.exports = Article;
